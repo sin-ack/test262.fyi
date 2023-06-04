171a551a3e68e85847248771a710d10e5a601440
sin-ack <sin-ack@users.noreply.github.com>
2023-06-04 21:03:51 +0000

    Make the result download script a bit nicer :^)
    1) Enable the unofficial "Bash strict mode"
    2) Always use a location relative to the script itself, so that the
       script can be run from any directory and work correctly.
    3) Use `mkdir -p` to avoid "already exists" errors

--- scripts/downloadResults.sh
+++ scripts/downloadResults.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
+set -euo pipefail
+
+BASE_DIR="$(realpath "$(dirname "$0")/..")"
+
 engines=( v8 sm jsc chakra graaljs hermes libjs engine262 qjs xs )
 
+mkdir -p "$BASE_DIR/results"
 for x in "${engines[@]}"
 do
   echo "$x"
 
-  curl -L -o "results/$x.zip" "https://nightly.link/CanadaHonk/test262.fyi/workflows/run/main/$x.zip"
+  curl -L -o "$BASE_DIR/results/$x.zip" "https://nightly.link/CanadaHonk/test262.fyi/workflows/run/main/$x.zip"
 
-  mkdir "results/$x"
-  unzip -o -d "results/$x" "results/$x.zip"
+  mkdir -p "$BASE_DIR/results/$x"
+  unzip -o -d "$BASE_DIR/results/$x" "$BASE_DIR/results/$x.zip"
 
-  rm "results/$x.zip"
+  rm "$BASE_DIR/results/$x.zip"
 done
